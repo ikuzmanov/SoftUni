5d23f388831c1c0130f3e9cf805402e5a8709671
Ivaylo <ivaylo.kuzmanov@gmail.com>
2021-11-09 12:02:37 +0200

    Dictionaries exercises

---
+++ Fundamentals module/Dictionaries - Lab/05. ASCII Values.py
@@ -0,0 +1,4 @@
+data = input().split(", ")
+
+my_dict = {key:ord(key) for key in data}
+print(my_dict)
--- main.py
+++
@@ -1,2 +0,0 @@
-classses = {"a":["Gosho", "Pesho", "Plamencho"], "b":[1,2,3]}
-print(classses["a"][2])
