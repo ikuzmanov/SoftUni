7e7d652caf344fb77584d5ba4e307527a28c33cb
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-01-10 21:00:00 +0200

    Django tutorial - part 5

--- djangoProject/polls/views.py
+++ djangoProject/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
 from django.views import generic
@@ -13,18 +14,29 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -39,6 +51,7 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
