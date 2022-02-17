34efd1d914bd83d36fd82160ef111b1c951488e8
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-02-17 17:03:43 +0200

    Exam Preparation

---
+++ Python Advanced/Еxam Preparation/03.  Shopping List.py
@@ -0,0 +1,31 @@
+def shopping_list(budget, **kwargs):
+    if budget < 100:
+        return "You do not have enough budget."
+
+    products = []
+    basket = set()
+
+    for product, price_and_quantity in kwargs.items():
+        if len(basket) == 5:
+            break
+        price, quantity = price_and_quantity
+        total_sum = price*quantity
+
+        if budget >= total_sum:
+            products.append(f"You bought {product} for {total_sum:.2f} leva")
+            basket.add(product)
+            budget -= total_sum
+
+    return "\n".join(products)
+
+
+print(shopping_list(104,
+                    cola=(1.20, 2),
+                    candies=(0.25, 15),
+                    bread=(1.80, 1),
+                    pie=(10.50, 5),
+                    tomatoes=(4.20, 1),
+                    milk=(2.50, 2),
+                    juice=(2, 3),
+                    eggs=(3, 1),
+                    ))
