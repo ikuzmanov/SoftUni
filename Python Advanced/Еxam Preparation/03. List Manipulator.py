b26dfe039b9cc91598f5be93bbd20b82478616f7
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-02-19 00:21:11 +0200

    Exam Preparation

--- Python Advanced/Еxam Preparation/03. List Manipulator.py
+++ Python Advanced/Еxam Preparation/03. List Manipulator.py
@@ -1,30 +1,30 @@
 def list_manipulator(numbers_list, command, location, *args):
     if command == "add":
+
         if location == "beginning":
             numbers_list = list(args) + numbers_list
             return numbers_list
+
         elif location == "end":
             numbers_list += args
-            return numbers_list
 
     elif command == "remove":
+
         if location == "beginning":
             if not args:
                 numbers_list = numbers_list[1::]
-                return numbers_list
             else:
                 numbers_to_remove, = args
                 del numbers_list[:-numbers_to_remove+1]
-                return numbers_list
 
         elif location == "end":
             if not args:
                 numbers_list.pop()
-                return numbers_list
             else:
                 numbers_to_remove, = args
                 del numbers_list[-numbers_to_remove:]
-                return numbers_list
+
+    return numbers_list
 
 print(list_manipulator([1,2,3], "remove", "end"))
 print(list_manipulator([1,2,3], "remove", "beginning"))
