582c88821d4348ea837c594d2d5b418f0d49933b
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-06-24 22:43:41 +0300

    Data definition and Datatypes

--- MySQL/01. Data-Definition-And-Datatypes-Exercises/01_data_types.sql
+++ MySQL/01. Data-Definition-And-Datatypes-Exercises/01_data_types.sql
@@ -255,3 +255,102 @@ VALUES
 (1,2,3),
 (2,2,3),
 (3,1,2);
+
+# 13. Basic Insert
+CREATE SCHEMA `soft_uni`;
+USE `soft_uni`;
+
+CREATE TABLE `towns`(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(30)
+);
+
+CREATE TABLE `addresses`(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `address_text` TEXT,
+    `town_id` INT,
+    CONSTRAINT fk_addresses_towns
+    FOREIGN KEY `addresses`(`town_id`) REFERENCES `towns`(`id`)
+);
+
+CREATE TABLE `departments`(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`name` VARCHAR(30)
+);
+
+CREATE TABLE `employees`(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(20),
+    `middle_name` VARCHAR(20),
+    `last_name` VARCHAR(20),
+    `job_title` VARCHAR(20),
+    `department_id` INT,
+    `hire_date` DATE,
+    `salary` DECIMAL(10,2),
+    `address_id` INT,
+	CONSTRAINT fk_employees_departments
+    FOREIGN KEY `employees`(`department_id`) REFERENCES `departments`(`id`),
+	CONSTRAINT fk_employees_addresses
+    FOREIGN KEY `employees`(`address_id`) REFERENCES `addresses`(`id`)
+);
+
+INSERT INTO `towns`(`name`)
+VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+INSERT INTO `departments`(`name`)
+VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+INSERT INTO `employees`(`first_name`, `middle_name`, `last_name`,`job_title`, `department_id`, `hire_date`, `salary`)
+VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+('Georgi ', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
+
+
+# 14. Basic Select All Fields
+SELECT * FROM `towns`;
+SELECT * FROM `departments`;
+SELECT * FROM `employees`;
+
+# 15. Basic Select All Fields and Order Them
+SELECT * FROM `towns`
+ORDER BY `name`;
+
+SELECT * FROM `departments`
+ORDER BY `name`;
+
+SELECT * FROM `employees`
+ORDER BY `salary` DESC;
+
+# 16. Basic Select Some Fields
+SELECT `name` FROM `towns`
+ORDER BY `name`;
+
+SELECT `name` FROM `departments`
+ORDER BY `name`;
+
+SELECT `first_name`, `last_name`, `job_title`, `salary` FROM `employees`
+ORDER BY `salary` DESC;
+
+# 17. Increase Employees Salary
+UPDATE `employees`
+SET
+`salary` = `salary` * 1.1
+WHERE
+`id` > 0;
+
+SELECT `salary` FROM `employees`;
+
+# 18. Delete All Records
+TRUNCATE TABLE `occupancies`;
