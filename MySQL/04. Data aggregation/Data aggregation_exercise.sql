a16730155d933a3e8e6e0ec141473981c084afa2
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-08-05 00:26:51 +0300

    Data aggregation exercise

---
+++ MySQL/04. Data aggregation/Data aggregation_exercise.sql
@@ -0,0 +1,21 @@
+#1. Records’ Count
+SELECT COUNT(id) AS 'count' FROM wizzard_deposits;
+
+#2.	 Longest Magic Wand
+SELECT MAX(magic_wand_size) AS 'longest_magic_wand' FROM wizzard_deposits;
+
+#3. Longest Magic Wand Per Deposit Groups
+SELECT deposit_group, MAX(magic_wand_size) AS 'longest_magic_wand' FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY  MAX(magic_wand_size) ASC, deposit_group ASC;
+
+#4. Smallest Deposit Group Per Magic Wand Size
+SELECT deposit_group FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY AVG(magic_wand_size)
+LIMIT 1;
+
+#5.	 Deposits Sum
+SELECT deposit_group, SUM(deposit_amount) AS 'total_sum' FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY SUM(deposit_amount) ASC;
