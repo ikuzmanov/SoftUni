3cc5a03929f159dbb10c9c0cb7ca43a2017e07ba
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-12-20 00:19:54 +0200

    Templates exercise

--- Python Web Basics/templates_demo/web/views.py
+++ Python Web Basics/templates_demo/web/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+def return_hello():
+    return "HELLO FROM FUNC"
 def index(request):
     context = {
         'title': 'My homepage',
         'body': 'This is the homepage of this project. Below you can find your random number for the day:',
-        'randnum': random.random()
+        'randnum': random.random(),
+        'randomlist': ['cat', 'dog', 'elephant', 'tiger'],
+        'hello_func': return_hello(),
+        # 'students': ['Pesho', 'Gosho', 'Ivan', 'Maria'],
+        'teachers': ['Dimitar', 'Radina', 'Petia']
     }
 
     return render(request, "web/index.html", context)
