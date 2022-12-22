f392aae466603316c4b03604128a6363c1edf398
Ivaylo <ivaylo.kuzmanov@gmail.com>
2022-12-22 22:33:54 +0200

    Templates exercise

--- Python Web Basics/templates_demo/web/urls.py
+++ Python Web Basics/templates_demo/web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from web.views import index
+from web.views import index, search
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("search", search, name="google search")
 ]
--- Python Web Basics/templates_demo/web/views.py
+++ Python Web Basics/templates_demo/web/views.py
@@ -1,12 +1,14 @@
 import random
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 
 def return_hello():
     return "HELLO FROM FUNC"
+
+
 def index(request):
     context = {
         'title': 'My homepage',
@@ -15,7 +17,12 @@ def index(request):
         'randomlist': ['cat', 'dog', 'elephant', 'tiger'],
         'hello_func': return_hello(),
         # 'students': ['Pesho', 'Gosho', 'Ivan', 'Maria'],
-        'teachers': ['Dimitar', 'Radina', 'Petia']
+        'teachers': ['Dimitar', 'Radina', 'Petia'],
+        'values': tuple(range(1, 50))
     }
 
     return render(request, "web/index.html", context)
+
+
+def search(request):
+    return redirect("https://google.com")
