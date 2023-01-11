33bbb76f7563fa0f562c3b350732e97e64cedbc7
Ivaylo <ivaylo.kuzmanov@gmail.com>
2023-01-11 20:39:12 +0200

    Models exercise

--- Python Web Basics/models_demos/models_demos/web/admin.py
+++ Python Web Basics/models_demos/models_demos/web/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
 
-from models_demos.web.models import Employee, NullBlankDemo
+from models_demos.web.models import Employee, NullBlankDemo, Department, Project, Category
 
 
 # Register your models here.
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'first_name', 'last_name', 'email', 'level')
+    list_filter = ('level', 'works_full_time')
+    search_fields = ('first_name', 'last_name')
+    fieldsets = (('Personal info',
+                  {'fields': ('first_name', 'last_name', 'age')}),
+                 ('Professional info',
+                  {'fields': ('level', 'years_of_experience')}),
+                 ('Company Info',
+                  {'fields': ('department', 'works_full_time', 'start_date')})
+                 )
 
 
 @admin.register(NullBlankDemo)
 class NullBlankDemoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
--- Python Web Basics/models_demos/models_demos/web/models.py
+++ Python Web Basics/models_demos/models_demos/web/models.py
@@ -2,22 +2,40 @@ from django.db import models
 
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"[ID: {self.id}] Name: {self.name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    code_name = models.CharField(max_length=10, unique=True)
+    deadline = models.DateField()
+
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=40)
     review = models.TextField()
-    years_of_experience = models.PositiveIntegerField
+    years_of_experience = models.PositiveIntegerField()
     start_date = models.DateField()
     email = models.EmailField(unique=True, editable=False)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     age = models.IntegerField(default=-1)
     works_full_time = models.BooleanField(null=False)
-    level = models.CharField(max_length=25,  choices=(
+    level = models.CharField(max_length=25, choices=(
         ('jr', 'Junior'),
         ('reg', 'Regular'),
         ('sr', 'Senior')
     ))
+
+    # One-to-many
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    # Many-to-many
+    projects = models.ManyToManyField(Project)
 
     @property
     def full_name(self):
@@ -26,6 +44,18 @@ class Employee(models.Model):
     def __str__(self):
         return f'ID: {self.pk}; Name: {self.full_name}'
 
+
+class AccessCard(models.Model):
+    employee = models.OneToOneField(Employee, on_delete=models.CASCADE,
+                                    primary_key=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=35)
+    parent_category = models.ForeignKey('Category', on_delete=models.RESTRICT, null=True, blank=True)
+
+    def __str__(self):
+        return f"Name:{self.name}"
 
 class NullBlankDemo(models.Model):
     x = models.IntegerField(
