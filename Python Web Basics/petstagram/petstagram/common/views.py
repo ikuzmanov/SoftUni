355f7655a8c8d4f34ca7ec5ba8e46df1379b340e
Ivaylo <ivaylo.kuzmanov@gmail.com>
2023-01-24 21:53:33 +0200

    Workshop exercise

--- Python Web Basics/petstagram/petstagram/common/views.py
+++ Python Web Basics/petstagram/petstagram/common/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render, redirect
-
+from django.urls import reverse
+from pyperclip import copy
 from petstagram.common.models import PhotoLike
+from petstagram.common.utils import get_photo_url, get_user_liked_photos
+from petstagram.core.photo_utils import apply_likes_count, apply_user_like_photo
 from petstagram.photos.models import Photo
 
 
 # Create your views here.
-def apply_user_like_photo(photo):
-    # TODO fix this for user when authentication is created
-    photo.is_liked_by_user = photo.likes_count > 0
-    return photo
-
-
-def apply_likes_count(photo):
-    photo.likes_count = photo.photolike_set.count()
-    return photo
 
 
 def index(request):
@@ -26,15 +20,18 @@ def index(request):
     return render(request, 'common/home-page.html', context)
 
 
-def get_user_liked_photos(photo_id):
-    # TODO fix when authentication
-    return PhotoLike.objects.filter(photo_id=photo_id)
-
-
 def like_photo(request, photo_id):
     user_liked_photos = get_user_liked_photos(photo_id)
     if user_liked_photos:
         user_liked_photos.delete()
     else:
         PhotoLike.objects.create(photo_id=photo_id)
-    return redirect(request.META['HTTP_REFERER'] + f'#photo-{photo_id}')
+    return redirect(get_photo_url(request, photo_id))
+
+
+def share_photo(request, photo_id):
+    photo_details_url = request.META['HTTP_HOST'] + reverse('details photo', kwargs={
+        'pk': photo_id
+    })
+    copy(photo_details_url)
+    return redirect(get_photo_url(request, photo_id))
