22018fd36eebcad9c79d6827669efcfda21bd8be
Ivaylo <ivaylo.kuzmanov@gmail.com>
2023-01-27 22:45:53 +0200

    Forms exercise

--- Python Web Basics/forms_demos/forms_demos/web/views.py
+++ Python Web Basics/forms_demos/forms_demos/web/views.py
@@ -4,20 +4,35 @@ from django.shortcuts import render
 from forms_demos.web.models import Person
 
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(max_length=30)
+class PersonForm(forms.Form):
+    OCCUPANCIES = (
+        (1, 'Child'),
+        (2, 'High schoolStudent'),
+        (3, 'Student'),
+        (4, 'Adult')
+    )
+    your_name = forms.CharField(max_length=30, help_text="Enter your name", widget = forms.TextInput(
+        attrs={
+            'class': 'form-control-lg',
+            'placeholder': 'Enter your name',
+        }
+    ))
+    age = forms.IntegerField(required=False, label='Your age', initial=0)
+    email = forms.EmailField()
+    occupancy = forms.ChoiceField(choices=OCCUPANCIES, widget=forms.RadioSelect())
+    story = forms.CharField(widget=forms.Textarea())
 
 
 # Create your views here.
 def index(request):
     name = None
     if request.method == 'GET':
-        form = NameForm()
+        form = PersonForm()
     else:  # request.method == 'POST'
-        form = NameForm(request.POST)
-        form.is_valid()
-        name = form.cleaned_data['your_name']
-        Person.objects.create(name=name)
+        form = PersonForm(request.POST)
+        if form.is_valid():  # 1. validates the form 2.fills 'cleaned_data'
+            name = form.cleaned_data['your_name']
+            Person.objects.create(name=name)
     context = {
         'form': form,
         'name': name,
