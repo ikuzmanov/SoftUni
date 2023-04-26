3606edfa03b1c6b9958c6dbd0f03bd795c3c2a32
ikuzmanov <ivaylo.kuzmanov@gmail.com>
2023-04-26 23:09:02 +0300

    Basic views to render html and creating base page to be extended

--- Python_Web_Basics/gamesplayapp/gamesplayapp/web/views.py
+++ Python_Web_Basics/gamesplayapp/gamesplayapp/web/views.py
@@ -5,40 +5,40 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'home-page.html')
 
 
 def create_profile(request):
-    pass
+    return render(request, 'profile/create-profile.html')
 
 
 def dashboard(request):
-    pass
+    return render(request, 'dashboard.html')
 
 
 def create_game(request):
-    pass
+    return render(request, 'game/create-game.html')
 
 
 def details_game(request):
-    pass
+    return render(request, 'game/details-game.html')
 
 
 def edit_game(request):
-    pass
+    return render(request, 'game/edit-game.html')
 
 
 def delete_game(request):
-    pass
+    return render(request, 'game/delete-game.html')
 
 
 def details_profile(request):
-    pass
+    return render(request, 'profile/details-profile.html')
 
 
 def edit_profile(request):
-    pass
+    return render(request, 'profile/edit-profile.html')
 
 
 def delete_profile(request):
-    pass
+    return render(request, 'profile/delete-profile.html')
